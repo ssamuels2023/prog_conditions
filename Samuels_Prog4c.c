83c96558c0a2b96bcb485645cb49545be807c731
ssamuels2023 <163223044+ssamuels2023@users.noreply.github.com>
2024-05-09 22:52:54 -0400

    Update Samuels_Prog4c.c

--- Samuels_Prog4c.c
+++ Samuels_Prog4c.c
@@ -2,8 +2,9 @@
 Sophie Samuels
 Program 4c
 DisplayPhones function for ordering a phone and determining if within budget
-
 compile, build, and execute code after completing the algorithm for prog 4
+The purpose of this program is to establish skills with use of math in C, 
+conditions, a while loop, and programmer defined functions
 COP2220 2.20.2024
 */
 
